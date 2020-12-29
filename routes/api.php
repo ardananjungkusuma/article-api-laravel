3bb599ba6d1af1852082f88abf2697c006339412
ardananjungkusuma <ardananjungkusuma@gmail.com>
2020-12-29 13:58:16 +0700

    First commit, auth using jwt

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+Route::namespace('Auth')->group(function () {
+    Route::post('register', 'RegisterController');
+    Route::post('login', 'LoginController');
+    Route::post('logout', 'LogoutController');
+});
+
+
+Route::get('user', 'UserController');
